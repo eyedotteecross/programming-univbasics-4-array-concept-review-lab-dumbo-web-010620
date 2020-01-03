804d48ab10f38eb4a3a855ebb97fd02c9dd81d0b
Anthony Holder <anthonyh202x@gmail.com>
2020-01-03 21:08:39 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,5 +1,6 @@
 require 'pry'
-def find_element_index (array,index) 
-  array[index]
-  binding.pry
+def find_element_index (array,value) 
+  array.map {|i| array[i] == value ? return value : nil}
+  
+  #binding.pry
 end
