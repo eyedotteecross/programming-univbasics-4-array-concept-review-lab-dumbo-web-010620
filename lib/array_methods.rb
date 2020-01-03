422843c5e8c353594d21e64e4fa27f209d57c4a5
Anthony Holder <anthonyh202x@gmail.com>
2020-01-03 21:41:37 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -2,12 +2,11 @@ require 'pry'
 
 def find_element_index (array,value) 
 array.index(value)
-
 end
 
 def find_max_value (array)
-i = 1
-max = array[0] 
+  i = 1
+  max = array[0] 
   while i < array.size do
     if max>array[i]
     max = array[i]
@@ -18,6 +17,3 @@ max
 end 
 
 
-def find_max_value (array)
-array.min  
-end 
