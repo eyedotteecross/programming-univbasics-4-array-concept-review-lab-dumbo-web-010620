5500a0325a76b6206a0754c74874ada3282a710d
Anthony Holder <anthonyh202x@gmail.com>
2020-01-03 21:44:46 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -8,7 +8,7 @@ def find_max_value (array)
   i = 1
   max = array[0] 
   while i < array.size 
-    if !(max>array[i])
+    if max<array[i]
     max = array[i]
     end
   i += 1
@@ -16,4 +16,14 @@ def find_max_value (array)
 max
 end 
 
+def find_min_value (array)
+  i = 1
+  min = array[0] 
+  while i < array.size 
+    if min>array[i]
+    min = array[i]
+    end
+  i += 1
+  end
+max
 
