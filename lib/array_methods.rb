d59a1ae7b93dc1aebe9fc219d0bedff566d49e18
Anthony Holder <anthonyh202x@gmail.com>
2020-01-03 21:25:54 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,11 @@
 require 'pry'
 def find_element_index (array,value) 
   array.index(value)
-  #binding.pry
 end
 
 def find_max_value (array)
 array.max 
+ binding.pry
 end 
 
 def find_max_value (array)
